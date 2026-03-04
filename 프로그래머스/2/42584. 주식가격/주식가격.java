fcd792f9c8e7c5bbf08708ddc49a9f2b58f5f834
Jeongmin Mo <jmmo0722@gmail.com>
2026-03-04 18:14:24 +0900

    [level 2] Title: 주식가격, Time: 32.25 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,30 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-        Deque<Integer> que = new ArrayDeque<>();
-        ArrayList<Integer> list = new ArrayList<>();
         
-        //que.add(prices[0]); // 초기값
-        for(int i = 0; i < n; i++){
+        int[] answer = new int[prices.length];
+        
+        Deque<Integer> que = new ArrayDeque<>();
+        
+        for(int i = 0; i < prices.length; i++){
             
-            while(!que.isEmpty() && prices[i] < prices[que.peek()]){
-                
-                int idx = que.pop();
-                answer[idx] = i - idx;
-                
+            while(!que.isEmpty() && prices[que.peek()] > prices[i]){
+                int index = que.pop();
+                answer[index] = i - index;
             }
-            
             que.push(i);
         }
         
+        // 남아있는 스텍들 정리
         while(!que.isEmpty()){
-                
-            int idx = que.pop();
-            answer[idx] = n - 1 - idx;
-                
+            
+            int index = que.pop();
+            answer[index] = prices.length - 1 - index;
         }
+
         return answer;
     }
 }
