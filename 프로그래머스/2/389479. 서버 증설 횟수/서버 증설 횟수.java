7efe7f2b1f2579166f61344c7390cf4dfbbdfb6c
Jeongmin Mo <jmmo0722@gmail.com>
2026-03-19 23:19:56 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,51 +1,27 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] players, int m, int k) {
+        int time = 24;
         int answer = 0;
+        //종료될 서버의 수
+        int[] server = new int[time+k];
+        //현재 시점에 활성화된 서버의 수
+        int activeServer = 0;
         
-        // 증설한 서버 <반납 시간, 갯수>
-        Map<Integer, Integer> server = new HashMap<>();
-        
-        for(int i = 0; i < players.length; i++){
+        for(int i = 0; i < time; i++){
+            //현재 시점에 활성화된 서버에 종료될 서버를 반영함
+           activeServer -= server[i];
+           //m명당 필요한 서버의 개수를 계산
+            int needServer = players[i] / m;
             
-            // i시간의 플레이어 수
-            int playersCount = players[i];
-            
-            // i시간 기준 증설된 서버 수
-            int currentServer = 0;
-            for(Integer count : server.values()){
-                currentServer += count;
+            //만약 활성화된 서버보다 필요한 서버의 수가 부족하다면 추가될 서버의 개수와 k시간 뒤 종료될 서버를 계산
+            if(needServer > activeServer){
+                int addServer = needServer - activeServer;
+                activeServer += addServer;
+                server[i+k] += addServer;
+                answer += addServer;
             }
-            
-            // 현재 증설한 서버 수가 감당 가능한지 체크
-            // 증설이 필요한 경우 Map<>의 합 n * m < playersCount
-            if(currentServer * m < playersCount && playersCount >= m){
-                int needsServer = (playersCount - (currentServer * m)) / m; // 몫
-//                 int restPlayers = (playersCount - (currentServer * m)) % m; // 나머지
-                
-//                 // 만약 나머지가 있다면 서버 +1
-//                 if(restPlayers > 0 && restPlayers < m) needsServer++;
-                
-                // Map<>에 추가
-                server.put(i+k, needsServer);
-                answer += needsServer;
-                
-                System.out.println(i + "~" + (i+1) + " | " + playersCount + " | " 
-                                   + (currentServer+needsServer) + " | " + needsServer + " | currentServer:" + currentServer);
-            }
-            
-            // 반납해야 할 서버 확인
-            for(Integer time : server.keySet()){
-                if(time == i+1){
-                    server.remove(time);
-                    System.out.println("삭제된 시간: " + time);                
-                    break; // Map<>은 for문 중에 값 변경이 일어나면 에러가 난다.
-                }
-            }
-            
         }
-        
         return answer;
     }
 }
