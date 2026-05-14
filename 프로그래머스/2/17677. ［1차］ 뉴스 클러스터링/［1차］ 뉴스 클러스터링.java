2835b466a7c325f282c3df423f4902978faba753
Jeongmin Mo <jmmo0722@gmail.com>
2026-05-14 17:44:31 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.86 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,47 +3,58 @@ import java.util.*;
 import java.util.regex.Pattern;
 class Solution {
     
-    public static ArrayList<String> parseString(String str){
-        
-        String st = str.toLowerCase();
-        
-        ArrayList<String> list = new ArrayList<>();
-        
-        for(int i = 0; i < str.length()-1; i++){
-            
-            if(Pattern.matches("^[a-z]{2}$", st.substring(i, i+2))){
-                list.add(st.substring(i, i+2));
-            }
-        }
-        
-        return list;
-    }
     public int solution(String str1, String str2) {
         
-        ArrayList<String> list1 = parseString(str1);
-        ArrayList<String> list2 = parseString(str2);
+        Map<String, Integer> mapStr1 = new HashMap<>();
+        Map<String, Integer> mapStr2 = new HashMap<>();
         
-        // 복사본
-        ArrayList<String> copy = new ArrayList<>(list2);
-        int count = 0;
-        int list1Size = list1.size();
-        int list2Size = list2.size();
-        
-        for(String s : copy){
-            
-            if(list1.contains(s)){
-                list1.remove(s);
-                count++;
+        // 모두 대문자로 변환
+        String upperStr1 = str1.toUpperCase();
+        String upperStr2 = str2.toUpperCase();
+
+        // 2글자씩 변환
+        for(int i = 0; i < upperStr1.length()-1; i++){
+            String value = upperStr1.substring(i, i+2);
+            if(check(value)) {
+                mapStr1.put(value, mapStr1.getOrDefault(value, 0) + 1);
             }
         }
         
-        int plusCount = list1Size + list2Size - count;
-        
-        if (plusCount == 0) {
-            return 65536; // A, B 모두 공집합인 경우
+        for(int i = 0; i < upperStr2.length()-1; i++){
+            String value = upperStr2.substring(i, i+2);
+            if(check(value)) {
+                mapStr2.put(value, mapStr2.getOrDefault(value, 0) + 1);
+            }
         }
         
-        double jaccard = (double) count / plusCount;
+        // map을 통해 교집합 계산
+        int same = 0;
+        for(String key : mapStr1.keySet()){
+            same += Math.min(mapStr1.getOrDefault(key, 0), mapStr2.getOrDefault(key, 0));
+        }
+        
+        // 합집합 계산
+        
+        int str1Count = 0;
+        for(int i : mapStr1.values()) str1Count += i;
+        
+        int str2Count = 0;
+        for(int i : mapStr2.values()) str2Count += i;
+        
+        int union = str1Count + str2Count - same;
+        
+        if(union == 0) return 65536;
+        double jaccard = (double) same / union;
         return (int) (jaccard * 65536);
+    }
+    
+    public boolean check(String value){
+        
+        char c1 = value.charAt(0);
+        char c2 = value.charAt(1);
+        
+        if('A' <= c1 && c1 <= 'Z' && 'A' <= c2 && c2 <= 'Z') return true;
+        
+        return false;
     }
 }
