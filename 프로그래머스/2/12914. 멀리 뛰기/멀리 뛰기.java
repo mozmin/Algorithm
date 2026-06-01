d815b81e9db35b5b34862d7c80724fd5b2d674c5
Jeongmin Mo <jmmo0722@gmail.com>
2026-06-01 22:51:41 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public long solution(int n) {
+        
+        if(n == 1) return 1;
+        long result = 0;
+        
+        // 점화식: dp(n) = dp(n-1) + dp(n-2)
+        int[] dist = new int[n+1];
+        
+        dist[1] = 1;
+        dist[2] = 2;
+        
+        if(n <= 2) return dist[n];
+        else{
+            for(int i = 3; i <= n; i++){
+                dist[i] = (dist[i-1] + dist[i-2]) % 1234567;
+            }
+        }
+        
+        return dist[n];
+    }
+
+}
