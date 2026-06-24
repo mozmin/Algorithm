f54f092b62720ccf1bf183cc72736ded011612b8
Jeongmin Mo <jmmo0722@gmail.com>
2026-06-24 23:00:12 +0900

    [level 2] Title: 피로도, Time: 2.91 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     
     static int max;
@@ -5,23 +7,23 @@ class Solution {
         
         max = 0;
         boolean[] visited = new boolean[dungeons.length];
-        dfs(k, 0, visited, dungeons);
+        dfs(k, dungeons, 0, visited);
         
         return max;
     }
     
-    public static void dfs(int k, int count, boolean[] visited, int[][] dungeons){
+    public void dfs(int k, int[][] dungeons, int count, boolean[] visited){
         
+
         max = Math.max(max, count);
-        
+
         for(int i = 0; i < dungeons.length; i++){
-            if(visited[i]) continue;
-            
-            if(k >= dungeons[i][0] && k >= dungeons[i][1]){
+            if(!visited[i] && k >= dungeons[i][0] && k >= dungeons[i][1]){
                 visited[i] = true;
-                dfs(k - dungeons[i][1], count+1, visited, dungeons);
+                dfs(k-dungeons[i][1], dungeons, count+1, visited);
                 visited[i] = false;
             }
         }
     }
+    
 }
